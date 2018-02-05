29dc9df36e0cbdb099a9725ea120ccc8eb0abe14
Honorio Vega <hvega@csumb.edu>
2018-02-04 23:19:07 -0800

    Show question on app launch

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
 
     override func didReceiveMemoryWarning() {
