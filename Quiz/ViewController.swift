0b71a30f6b7b7c62342f929457149423f747af66
Honorio Vega <hvega@csumb.edu>
2018-02-04 22:52:23 -0800

    udpate

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
